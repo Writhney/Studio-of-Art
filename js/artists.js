84d076a20f0bacfc84b8f242d1d0aa8e4b04847c
kely123 <kely.piscis.97@gmail.com>
2022-03-07 14:25:04 -0500

    Listado de artistas

--- js/artists.js
+++ js/artists.js
@@ -1,6 +1,6 @@
 function addArtists() {
-    let artista= {
-        nombre : document.getElementById("nombre").value,
+    let artista = {
+        nombre: document.getElementById("nombre").value,
         imagen: document.getElementById("imagen").value
     }
     if (localStorage.getItem('artistas') === null) {
@@ -14,3 +14,22 @@ function addArtists() {
     }
 }
 
+function listArtists() {
+    let artistas = JSON.parse(localStorage.getItem('artistas'));
+    let artitasView = document.getElementById('table');
+    artitasView.innerHTML = '';
+    for (let i = 0; i < artistas.length; i++) {
+        let nombre = artistas[i].nombre;
+        let imagen = artistas[i].imagen
+
+        artitasView.innerHTML += `
+        <div class="card">
+            <img src="${imagen}" alt="Avatar" style="width:30%">
+            <div class="container">
+                <h4><b>${nombre}</b></h4> 
+                <p>Architect & Engineer</p>
+            </div>
+        </div>`;
+    }
+}
+listArtists()
